14fcf6bc86c96ab122ffd7e695e7df5ef2ca04c9
Anders S. Christensen <andersx@nano.ku.dk>
2016-10-07 15:12:22 +0200

    Updated code, added documentation

--- ezpickle/ezpickle.py
+++ ezpickle/ezpickle.py
@@ -1,43 +1,36 @@
 #!/usr/bin/env python2
 
-# MIT License
-# 
-# Copyright (c) 2016 Anders Steen Christensen
-# 
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-# 
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-# 
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
+"""
+Short wrapper functions to easily handle opening and 
+saving Python objects to files in cPickle formats.
 
+"""
 import cPickle
 
 def load_pickle(filename):
+    """ Returns Python data from a cPickle formatted file.
 
-    f = open(filename,"rb")
-    p = cPickle.load(f)
-    f.close()
-    return(p)
+    Arguments:
+    filename -- name of the file to be parsed.
+
+    """
+    with open(filename, "rb") as f:
+        p = cPickle.load(f)
+
+    return p
 
 def save_pickle(data, filename):
+    """ Saves about any Python data into a cPickle formatted file.
 
-    f = open(filename,"wb")
-    cPickle.dump(data, f, protocol=2)
-    f.close()
+    Arguments:
+    data -- the data or object to be saved.
+    filename -- name of the file to be parsed.
 
+    NOTE: Will overwrites any pre-existing file with the
+    name filename.
 
+    """
 
-
+    with open(filename, "rb") as f:
+        cPickle.dump(data, f, protocol=2)
 
