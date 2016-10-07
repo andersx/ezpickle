e4be9aa704b66022216f3b091b67bd017f0c425e
Anders S. Christensen <andersx@nano.ku.dk>
2016-10-07 15:00:33 +0200

    Added initial code

---
+++ ezpickle/__init__.py
@@ -0,0 +1,10 @@
+from ezpickle import *
+
+__author__ = "Anders S. Christensen"
+__copyright__ = "Copyright 2016"
+__credits__ = ["Anders S. Christensen (2016) https://github.com/andersx/ezpickle"
+__license__ = "MIT"
+__version__ = "1.0.0"
+__maintainer__ = "Anders S. Christensen"
+__email__ = "andersbiceps@gmail.com"
+__status__ = "Production"
